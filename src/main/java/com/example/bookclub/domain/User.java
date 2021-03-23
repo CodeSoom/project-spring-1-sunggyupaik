afb3100e2e1b3ede457b99e96d2fcd93102bc061
Gyu <melon9751@naver.com>
2021-03-24 00:03:26 +0900

    사용자 엔티티를 추가하라
    기존에 사용자 조회 서비스는 하드코딩으로 사용자를 조회했습니다.
    
    따라서, 사용자 정보를 담은 엔티티 클래스를 추가하였습니다.

---
+++ src/main/java/com/example/bookclub/domain/User.java
@@ -0,0 +1,34 @@
+package com.example.bookclub.domain;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString
+@Builder
+public class User {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    private String nickname;
+
+    private String password;
+
+    private String profileImage;
+
+    private boolean deleted;
+}
