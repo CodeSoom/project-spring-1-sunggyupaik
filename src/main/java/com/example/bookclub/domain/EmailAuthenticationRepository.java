7d23c000f63d6c109186ec907fec64f3153f24d3
Gyu <melon9751@naver.com>
2021-03-25 15:02:26 +0900

    이메일로 인증번호를 조회하는 쿼리를 추가하라
    이메일로 인증번호를 조회하는 쿼리를 추가합니다.

--- src/main/java/com/example/bookclub/domain/EmailAuthenticationRepository.java
+++ src/main/java/com/example/bookclub/domain/EmailAuthenticationRepository.java
@@ -1,5 +1,9 @@
 package com.example.bookclub.domain;
 
+import java.util.Optional;
+
 public interface EmailAuthenticationRepository {
     EmailAuthentication save(EmailAuthentication emailAuthentication);
+
+    Optional<EmailAuthentication> findByEmail(String email);
 }
--- src/main/java/com/example/bookclub/infra/JpaEmailAuthenticationRepository.java
+++ src/main/java/com/example/bookclub/infra/JpaEmailAuthenticationRepository.java
@@ -4,7 +4,11 @@ import com.example.bookclub.domain.EmailAuthentication;
 import com.example.bookclub.domain.EmailAuthenticationRepository;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface JpaEmailAuthenticationRepository
         extends EmailAuthenticationRepository, CrudRepository<EmailAuthentication, Long> {
     EmailAuthentication save(EmailAuthentication emailAuthentication);
+
+    Optional<EmailAuthentication> findByEmail(String email);
 }
