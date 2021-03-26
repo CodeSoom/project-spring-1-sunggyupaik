17e32401350097adfe0e8cc15c19ea818c04c184
Gyu <melon9751@naver.com>
2021-03-27 08:04:13 +0900

    스터디 생성 테스트를개선하라
    새롭게 추가한 서비스 내용과 Dto를 이용해 given과 mock 내용을 개선했습니다.

--- src/test/java/com/example/bookclub/controllers/StudyApiControllerTest.java
+++ src/test/java/com/example/bookclub/controllers/StudyApiControllerTest.java
@@ -1,13 +1,18 @@
 package com.example.bookclub.controllers;
 
+import com.example.bookclub.application.StudyService;
 import com.example.bookclub.domain.Day;
+import com.example.bookclub.domain.Study;
 import com.example.bookclub.domain.StudyState;
 import com.example.bookclub.domain.Zone;
+import com.example.bookclub.dto.StudyCreateDto;
+import com.example.bookclub.dto.StudyResultDto;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -16,12 +21,16 @@ import org.springframework.web.filter.CharacterEncodingFilter;
 
 import java.time.LocalDate;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(StudyApiController.class)
 class StudyApiControllerTest {
+    private static final Long EXISTED_ID = 1L;
     private static final String SETUP_NAME = "name";
     private static final String SETUP_DESCRIPTION = "description";
     private static final String SETUP_CONTACT = "contact";
@@ -31,7 +40,7 @@ class StudyApiControllerTest {
     private static final Day SETUP_DAY = Day.MONDAY;
     private static final String SETUP_STARTTIME = "13:00";
     private static final String SETUP_ENDTIME = "15:30";
-    private static final StudyState SETUP_STATUS = StudyState.OPEN;
+    private static final StudyState SETUP_STUDYSTATE = StudyState.OPEN;
     private static final Zone SETUP_ZONE = Zone.A01;
 
     @Autowired
@@ -40,8 +49,15 @@ class StudyApiControllerTest {
     @Autowired
     private WebApplicationContext ctx;
 
+    @MockBean
+    private StudyService studyService;
+
     @Autowired
     private ObjectMapper objectMapper;
+
+    private Study setUpStudy;
+
+    private StudyResultDto studyResultDto;
 
     @BeforeEach
     void setUp() {
@@ -49,16 +65,38 @@ class StudyApiControllerTest {
                 .addFilters(new CharacterEncodingFilter("UTF-8", true))
                 .alwaysDo(print())
                 .build();
+
+        setUpStudy = Study.builder()
+                .id(EXISTED_ID)
+                .name(SETUP_NAME)
+                .description(SETUP_DESCRIPTION)
+                .contact(SETUP_CONTACT)
+                .size(SETUP_SIZE)
+                .startDate(SETUP_STARTDATE)
+                .endDate(SETUP_ENDDATE)
+                .startTime(SETUP_STARTTIME)
+                .endTime(SETUP_ENDTIME)
+                .day(SETUP_DAY)
+                .studyState(SETUP_STUDYSTATE)
+                .zone(SETUP_ZONE)
+                .build();
+
+        studyResultDto = StudyResultDto.of(setUpStudy);
     }
 
     @Test
     void createWithValidateAttribute() throws Exception {
+        given(studyService.createStudy(any(StudyCreateDto.class))).willReturn(studyResultDto);
+
         mockMvc.perform(
                 post("/api/study")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content("{}")
+                .content(objectMapper.writeValueAsString(setUpStudy))
         )
                 .andDo(print())
-                .andExpect(status().isCreated());
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("id").value(studyResultDto.getId()))
+                .andExpect(jsonPath("name").value(studyResultDto.getName()))
+                .andExpect(jsonPath("description").value(studyResultDto.getDescription()));
     }
 }
