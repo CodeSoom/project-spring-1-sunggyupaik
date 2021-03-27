57896caca4c470734fbadc188812762836d5fb81
Gyu <melon9751@naver.com>
2021-03-28 00:04:29 +0900

    스터디 수정 주소 경로를 추가하라
    스터디 수정이 가능하도록 주소 경로를 추가합니다.

--- src/main/java/com/example/bookclub/controllers/StudyController.java
+++ src/main/java/com/example/bookclub/controllers/StudyController.java
@@ -1,21 +1,37 @@
 package com.example.bookclub.controllers;
 
+import com.example.bookclub.application.StudyService;
 import com.example.bookclub.domain.Day;
+import com.example.bookclub.domain.Study;
 import com.example.bookclub.domain.StudyState;
 import com.example.bookclub.domain.Zone;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/studys")
 public class StudyController {
+    private final StudyService studyService;
+
+    public StudyController(StudyService studyService) {
+        this.studyService = studyService;
+    }
+
     @GetMapping("/save")
     public String studySave(Model model) {
         model.addAttribute("day", Day.getAllDays());
         model.addAttribute("studyState", StudyState.getAllStudyStates());
         model.addAttribute("zone", Zone.getAllZones());
         return "studys/studys-save";
+    }
+
+    @GetMapping("/update/{id}")
+    public String studyUpdate(@PathVariable Long id, Model model) {
+        Study study = studyService.getStudy(id);
+        model.addAttribute("study", study);
+        return "studys/studys-update";
     }
 }
