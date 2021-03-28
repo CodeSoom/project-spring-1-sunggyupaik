edf7f7268dc671c00bcce6e3a3e0ba1fbccb90d0
Gyu <melon9751@naver.com>
2021-03-28 17:28:25 +0900

    베스트셀러 목록 조회를 개선하라
    베스트셀러 목록 조회 요청에서 요청하는 행동을 서비스의 함수로 만들도록
    개선합니다. 요청에서는 목록을 조회하도록 서비스에 요청만 하면 됩니다.

--- src/main/java/com/example/bookclub/application/BookService.java
+++ src/main/java/com/example/bookclub/application/BookService.java
@@ -3,6 +3,9 @@ package com.example.bookclub.application;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 
@@ -15,5 +18,19 @@ public class BookService {
         String address = "http://book.interpark.com/api/bestSeller.api?key=" +
                 apikey + "&categoryId=122&output=json";
         return new URL(address);
+    }
+
+    public String getBestSellers() throws IOException {
+        URL url = getBestSellerUrl();
+
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()))) {
+            StringBuilder sb = new StringBuilder();
+            String line;
+
+            while ((line = br.readLine()) != null) {
+                sb.append(line);
+            }
+            return sb.toString();
+        }
     }
 }
--- src/main/java/com/example/bookclub/controllers/BookApiController.java
+++ src/main/java/com/example/bookclub/controllers/BookApiController.java
@@ -5,10 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.URL;
 
 @RestController
 @RequestMapping("/api/book")
@@ -21,16 +18,6 @@ public class BookApiController {
 
     @GetMapping("/bestseller")
     public String lists() throws IOException {
-        URL url = bookService.getBestSellerUrl();
-
-        try (BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()))) {
-            StringBuilder sb = new StringBuilder();
-            String line;
-
-            while ((line = br.readLine()) != null) {
-                sb.append(line);
-            }
-            return sb.toString();
-        }
+        return bookService.getBestSellers();
     }
 }
