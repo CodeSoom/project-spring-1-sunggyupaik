c71f45eb6b9d702155bb0b6e1f09f3a6d96ca57c
Gyu <melon9751@naver.com>
2021-03-29 09:58:50 +0900

    홈페이지 메인 화면에 스터디 목록 전달을 추가하라
    Model을 사용하여 홈페이지 메인화면에, 스터디 목록이 담긴 리스트를
    전송하도록 추가합니다.
    
    현재는 홈페이지 메인화면에 목록이 나오지만, 나중에 스터디 게시판으로
    옮겨야합니다.

--- src/main/java/com/example/bookclub/controllers/HomeController.java
+++ src/main/java/com/example/bookclub/controllers/HomeController.java
@@ -1,12 +1,26 @@
 package com.example.bookclub.controllers;
 
+import com.example.bookclub.application.StudyService;
+import com.example.bookclub.domain.Study;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
+    private StudyService studyService;
+
+    public HomeController(StudyService studyService) {
+        this.studyService = studyService;
+    }
+
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
+        List<Study> lists = studyService.getStudies();
+
+        model.addAttribute("study", lists);
         return "index";
     }
 }
