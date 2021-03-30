0189875565ad58f69e62e29d7dc11d04efcb243f
Gyu <melon9751@naver.com>
2021-03-31 00:27:36 +0900

    스프링 시큐리티 설정을 추가하라
    gradle에 스프링 시큐리티를 추가하고 보안 설정 함수를생성합 니다.
    현재는 호출 함수만 정의하였으므로 나중에 필터 내용과 토큰을
    추가해야합니다.

---
+++ src/main/java/com/example/bookclub/config/SecurityJavaConfig.java
@@ -0,0 +1,28 @@
+package com.example.bookclub.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+
+@Configuration
+public class SecurityJavaConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .headers()
+                .frameOptions().disable()
+                .and()
+                .addFilter()
+                .addFilterBefore()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .exceptionHandling()
+                .authenticationEntryPoint(
+                        new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED));
+    }
+}
