dd8eecb83f2a98fe6ab3bc24aee2da4c17f187ec
Gyu <melon9751@naver.com>
2021-03-31 08:16:26 +0900

    로그인 요청을 추가하라
    로그인 요청을 추가합니다. 현재 리턴값이 하드코딩 되어있으므로,
    나중에 로그인 서비스를 이용하도록 개선해야 합니다.

--- src/main/java/com/example/bookclub/controllers/SessionController.java
+++ src/main/java/com/example/bookclub/controllers/SessionController.java
@@ -1,7 +1,19 @@
 package com.example.bookclub.controllers;
 
+import com.example.bookclub.dto.SessionCreateDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/session")
 public class SessionController {
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public String login(@RequestBody SessionCreateDto sessionCreateDto) {
+        return "{\"accessToken\":\"12345678\"}";
+    }
 }
