6bad9fc7e87b720cf42a50a778ace698ed7028ce
Gyu <melon9751@naver.com>
2021-03-31 11:50:14 +0900

    JJWT 토큰 생성 유틸을 추가하라
    JJWT 토큰 생성을 추가합니다.
    
    현재 하드코딩을 리턴하므로 나중에 JJWT를 이용하도록 개선해야 합니다.

--- src/main/java/com/example/bookclub/utils/JwtUtil.java
+++ src/main/java/com/example/bookclub/utils/JwtUtil.java
@@ -1,5 +1,11 @@
 package com.example.bookclub.utils;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class JwtUtil {
 
+    public String encode(Long id, String email) {
+        return "setUpToken";
+    }
 }
