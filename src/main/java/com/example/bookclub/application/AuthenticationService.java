432939a8b96078802e7d5232b05c41a8a4b9fdb4
Gyu <melon9751@naver.com>
2021-03-31 10:58:35 +0900

    토큰 생성 서비스를 추가하라
    토큰 생성 서비스를 추가합니다.
    
    현재는 하드코딩으로 리턴하므로 토큰 생성 개선이 필요합니다.

--- src/main/java/com/example/bookclub/application/AuthenticationService.java
+++ src/main/java/com/example/bookclub/application/AuthenticationService.java
@@ -3,6 +3,7 @@ package com.example.bookclub.application;
 import com.example.bookclub.domain.User;
 import com.example.bookclub.domain.UserRepository;
 import com.example.bookclub.dto.SessionCreateDto;
+import com.example.bookclub.dto.SessionResultDto;
 import com.example.bookclub.errors.AuthenticationBadRequestException;
 import org.springframework.stereotype.Service;
 
@@ -12,6 +13,14 @@ public class AuthenticationService {
 
     public AuthenticationService(UserRepository userRepository) {
         this.userRepository = userRepository;
+    }
+
+    public SessionResultDto createToken(SessionCreateDto sessionCreateDto) {
+        User user = authenticateUser(sessionCreateDto);
+
+        return SessionResultDto.builder()
+                .accessToken("setUpaccessToken")
+                .build();
     }
 
     public User authenticateUser(SessionCreateDto sessionCreateDto) {
