278ca4371dbb33f81f8fa34ffa65cd2d10c48374
Gyu <melon9751@naver.com>
2021-03-31 09:40:24 +0900

    사용자 인증 서비스를 추가하라
    사용자 인증을 서비스를 추가합니다. 현재 이메일과 비밀번호가 하드코딩으로
    리턴되기 때문에 나중에 저장소를 이용하도록 개선해야 합니다.

--- src/main/java/com/example/bookclub/application/AuthenticationService.java
+++ src/main/java/com/example/bookclub/application/AuthenticationService.java
@@ -1,8 +1,22 @@
 package com.example.bookclub.application;
 
+import com.example.bookclub.domain.User;
+import com.example.bookclub.domain.UserRepository;
+import com.example.bookclub.dto.SessionCreateDto;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthenticationService {
+    private final UserRepository userRepository;
 
+    public AuthenticationService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User authenticateUser(SessionCreateDto sessionCreateDto) {
+        return User.builder()
+                .email("setUpEmail")
+                .password("12345678")
+                .build();
+    }
 }
