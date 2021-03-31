7477f3ef401f10e6120c832cda0735afff51d97e
Gyu <melon9751@naver.com>
2021-03-31 14:48:59 +0900

    로그인 요청을 개선하라
    기존의 하드코딩에서 사용자 인증 서비스를 이용하여 로그인하도록
    개선합니다.

--- src/main/java/com/example/bookclub/controllers/SessionController.java
+++ src/main/java/com/example/bookclub/controllers/SessionController.java
@@ -1,6 +1,8 @@
 package com.example.bookclub.controllers;
 
+import com.example.bookclub.application.AuthenticationService;
 import com.example.bookclub.dto.SessionCreateDto;
+import com.example.bookclub.dto.SessionResultDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,9 +13,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/session")
 public class SessionController {
+    private final AuthenticationService authenticationService;
+
+    public SessionController(AuthenticationService authenticationService) {
+        this.authenticationService = authenticationService;
+    }
+
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public String login(@RequestBody SessionCreateDto sessionCreateDto) {
-        return "{\"accessToken\":\"12345678\"}";
+    public SessionResultDto login(@RequestBody SessionCreateDto sessionCreateDto) {
+        return authenticationService.createToken(sessionCreateDto);
     }
 }
