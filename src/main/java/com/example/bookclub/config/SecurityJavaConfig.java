0a4637af2b79109cda67c506ac92006c3aa54a38
Gyu <melon9751@naver.com>
2021-04-04 12:07:35 +0900

    자바 시큐리티 설정을 개선하라
    기존에 있던 인증서비스에 새롭게 DetailsService를 구현한 클래스를
    추가합니다.

--- src/main/java/com/example/bookclub/config/SecurityJavaConfig.java
+++ src/main/java/com/example/bookclub/config/SecurityJavaConfig.java
@@ -3,6 +3,7 @@ package com.example.bookclub.config;
 import com.example.bookclub.application.AuthenticationService;
 import com.example.bookclub.filters.AuthenticationErrorFilter;
 import com.example.bookclub.filters.JwtAuthenticationFilter;
+import com.example.bookclub.security.UserAuthenticationService;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -15,15 +16,18 @@ import javax.servlet.Filter;
 @Configuration
 public class SecurityJavaConfig extends WebSecurityConfigurerAdapter {
     private final AuthenticationService authenticationService;
+    private final UserAuthenticationService userAuthenticationService;
 
-    public SecurityJavaConfig(AuthenticationService authenticationService) {
+    public SecurityJavaConfig(AuthenticationService authenticationService,
+                              UserAuthenticationService userAuthenticationService) {
         this.authenticationService = authenticationService;
+        this.userAuthenticationService = userAuthenticationService;
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         Filter authenticationFilter = new JwtAuthenticationFilter(
-                authenticationManager(),authenticationService);
+                authenticationManager(), authenticationService, userAuthenticationService);
 
         Filter authenticationErrorFilter = new AuthenticationErrorFilter();
 
