f7cb7376eaeefe5d2d46be7d9b18ad4fea408008
Gyu <melon9751@naver.com>
2021-04-04 11:29:05 +0900

    사용자 인증 시비스를 추가하라
    사용자 인증 시, 이메일로 사용자를 조회하는 서비스를 추가합니다.
    
    주어진 이메일에 사용자가 존재하지 않는 예외를 추가해야 합니다.

---
+++ src/main/java/com/example/bookclub/security/UserAuthenticationService.java
@@ -0,0 +1,50 @@
+package com.example.bookclub.security;
+
+import com.example.bookclub.domain.Role;
+import com.example.bookclub.domain.RoleRepository;
+import com.example.bookclub.domain.User;
+import com.example.bookclub.domain.UserRepository;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class UserAuthenticationService implements UserDetailsService {
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+
+    public UserAuthenticationService(UserRepository userRepository,
+                                     RoleRepository roleRepository) {
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = getUser(email);
+        List<GrantedAuthority> authorities = getAllAuthorities(email);
+
+        return UserAuthentication.builder()
+                .user(user)
+                .authorities(authorities)
+                .build();
+    }
+
+    public User getUser(String email) {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> UserEmailNotFoundException(email));
+    }
+
+    public List<GrantedAuthority> getAllAuthorities(String email) {
+        List<Role> roles = roleRepository.findAllByEmail(email);
+        return roles.stream()
+                .map(role -> new SimpleGrantedAuthority(role.getName()))
+                .collect(Collectors.toList());
+    }
+}
