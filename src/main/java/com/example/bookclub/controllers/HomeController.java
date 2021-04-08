bdd4a6079ec777a05349a17cdbb04a2c139a0b6f
Gyu <melon9751@naver.com>
2021-04-08 14:53:57 +0900

    화면에서 상단에 고정 메뉴바를 추가하라
    화면에서 상단에 로그인, 로그아웃,회원가, 스터디생성이 있는 메뉴바를
    추가합니다.

--- src/main/java/com/example/bookclub/controllers/HomeController.java
+++ src/main/java/com/example/bookclub/controllers/HomeController.java
@@ -11,7 +11,7 @@ public class HomeController {
     @GetMapping("/")
     public String home(@CurrentAccount Account account, Model model) {
         if(account != null) {
-            model.addAttribute("userName", account.getEmail());
+            model.addAttribute("user", account);
         }
         return "index";
     }
