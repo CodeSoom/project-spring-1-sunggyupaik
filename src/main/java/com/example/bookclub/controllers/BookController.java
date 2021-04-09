4cbc055838e7b8e7b4480060b22684051eeb934d
Gyu <melon9751@naver.com>
2021-04-09 09:33:37 +0900

    도서 목록 조회 경로를 개선하라
    베스트셀러, 신간, 추천도서 목록을 조회하는 경로를 추가합니다.

--- src/main/java/com/example/bookclub/controllers/BookController.java
+++ src/main/java/com/example/bookclub/controllers/BookController.java
@@ -1,6 +1,7 @@
 package com.example.bookclub.controllers;
 
 import com.example.bookclub.application.BookService;
+import com.example.bookclub.domain.BookType;
 import org.json.simple.JSONArray;
 import org.json.simple.parser.ParseException;
 import org.springframework.stereotype.Controller;
@@ -11,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import java.io.IOException;
 
 @Controller
-@RequestMapping("/books/bestseller")
+@RequestMapping("/books")
 public class BookController {
     private final BookService bookService;
 
@@ -19,9 +20,23 @@ public class BookController {
         this.bookService = bookService;
     }
 
-    @GetMapping
-    public String lists(Model model) throws IOException, ParseException {
-        JSONArray bestSellers = bookService.getBestSellers();
+    @GetMapping("/bestseller")
+    public String booksBestSellerLists(Model model) throws IOException, ParseException {
+        JSONArray bestSellers = bookService.getBookLists(BookType.BESTSELLER);
+        model.addAttribute("book", bestSellers);
+        return "books/books-lists";
+    }
+
+    @GetMapping("/recommend")
+    public String booksRecommendLists(Model model) throws IOException, ParseException {
+        JSONArray bestSellers = bookService.getBookLists(BookType.RECOMMEND);
+        model.addAttribute("book", bestSellers);
+        return "books/books-lists";
+    }
+
+    @GetMapping("/new")
+    public String booksNewLists(Model model) throws IOException, ParseException {
+        JSONArray bestSellers = bookService.getBookLists(BookType.NEW);
         model.addAttribute("book", bestSellers);
         return "books/books-lists";
     }
