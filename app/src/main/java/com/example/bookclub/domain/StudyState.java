d118a3ff57a36d0ee15ea1a9e28fbd04d7720745
Gyu <melon9751@naver.com>
2021-04-18 13:13:10 +0900

    스터디 상태를 개선하라
    스터디 상태에서 END의 의미를 완료에서 종료로 개선합니다.

--- app/src/main/java/com/example/bookclub/domain/StudyState.java
+++ app/src/main/java/com/example/bookclub/domain/StudyState.java
@@ -7,7 +7,7 @@ import java.util.stream.Collectors;
 public enum StudyState implements EnumMapperType {
     OPEN("모집중"),
     CLOSE("마감"),
-    END("완료");
+    END("종료");
 
     private String studyState;
 
