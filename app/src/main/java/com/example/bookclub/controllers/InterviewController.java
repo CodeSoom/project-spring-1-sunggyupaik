6102d0a628df093b99fda4eda4682a562345fb87
Gyu <melon9751@naver.com>
2021-04-22 21:01:33 +0900

    인터뷰 목록 화면 요청을 추가하라
    인터뷰 목록 화면을 확인할 수 있는 페이지에 요청을 추가합니다.
    
    모든 인터뷰를 저장소에서 조회하는 서비스를 추가해야 합니다.

--- app/src/main/java/com/example/bookclub/controllers/InterviewController.java
+++ app/src/main/java/com/example/bookclub/controllers/InterviewController.java
@@ -1,9 +1,15 @@
 package com.example.bookclub.controllers;
 
 import com.example.bookclub.application.InterviewService;
+import com.example.bookclub.domain.Account;
+import com.example.bookclub.domain.Interview;
+import com.example.bookclub.security.CurrentAccount;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/interviews")
@@ -14,8 +20,24 @@ public class InterviewController {
         this.interviewService = interviewService;
     }
     @GetMapping
-    public String interviewLists() {
+    public String interviewLists(@CurrentAccount Account account,
+                                 Model model) {
+        if(account != null) {
+            checkTopMenu(account, model);
+        }
+        List<Interview> lists = interviewService.getInterviews();
+        model.addAttribute("interviews", lists);
         //List<Interview> list = interviewService.crawlAllInterviews();
         return "interviews/interviews-list";
+    }
+
+    private void checkTopMenu(@CurrentAccount Account account, Model model) {
+        model.addAttribute("account", account);
+        if (account.isMangerOf(account.getStudy())) {
+            model.addAttribute("studyManager", account.getStudy());
+        }
+        if (account.isApplierOf(account.getStudy())) {
+            model.addAttribute("studyApply", account.getStudy());
+        }
     }
 }
