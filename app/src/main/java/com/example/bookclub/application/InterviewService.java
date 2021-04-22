55eddf69699200b32f573fdf4f22af50780bd9b4
Gyu <melon9751@naver.com>
2021-04-22 20:58:03 +0900

    인터뷰를 크롤링하는 스케쥴을 추가하라
    매일 밤 11시 50분에 해당 날짜에 추가되어있는 인터뷰를 크롤링하도록
    서비스에 추가합니다.

--- app/src/main/java/com/example/bookclub/application/InterviewService.java
+++ app/src/main/java/com/example/bookclub/application/InterviewService.java
@@ -7,6 +7,7 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -71,5 +72,43 @@ public class InterviewService {
             e.printStackTrace();
         }
         return list;
+    }
+
+    @Scheduled(cron = "0 50 23 * * *")
+    public Interview crawlInterview() {
+        Interview interview = null;
+        try {
+            String url = "https://www.kyobobook.co.kr/author/info/AuthorInterViewMore.laf?perPage=20&targetPage=1";
+            Connection conn = Jsoup.connect(url);
+            Document doc = conn.get();
+
+            Elements interviewsLiElements = doc.select(".list_author_interview > li");
+
+            for(Element interviewElement : interviewsLiElements) {
+                Element photoChildElement = interviewElement.getElementsByClass("photo").first().child(0);
+                String boardId = photoChildElement.attr("href").split(",")[1].trim();;
+                String interviewUrl = "http://news.kyobobook.co.kr/people/interviewView.ink?orderclick=&sntn_id=" + boardId.substring(1, boardId.length()-1);
+                String imgUrl = interviewElement.select(".photo a img").attr("src").trim();
+                String author = interviewElement.select(".author a").text().trim();
+                String title = interviewElement.select(".title a").text().trim();
+                String date = interviewElement.select(".info").text().split("\\|")[0].trim();
+                String content = interviewElement.select(".detail").text().split("더보기")[0];
+
+                if(LocalDate.parse(date, DateTimeFormatter.ISO_DATE).isEqual(LocalDate.now())) {
+                    interview = Interview.builder()
+                            .interviewUrl(interviewUrl)
+                            .imgUrl(imgUrl)
+                            .author(author)
+                            .title(title)
+                            .date(LocalDate.parse(date, DateTimeFormatter.ISO_DATE))
+                            .content(content)
+                            .build();
+                    interviewRepository.save(interview);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return interview;
     }
 }
--- app/src/main/java/com/example/bookclub/controllers/InterviewController.java
+++ app/src/main/java/com/example/bookclub/controllers/InterviewController.java
@@ -1,12 +1,9 @@
 package com.example.bookclub.controllers;
 
 import com.example.bookclub.application.InterviewService;
-import com.example.bookclub.domain.Interview;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.List;
 
 @Controller
 @RequestMapping("/interviews")
@@ -18,7 +15,7 @@ public class InterviewController {
     }
     @GetMapping
     public String interviewLists() {
-        List<Interview> list = interviewService.crawlAllInterviews();
+        //List<Interview> list = interviewService.crawlAllInterviews();
         return "interviews/interviews-list";
     }
 }
