64653c229161f6a67b8fcd04731beb471261a938
Gyu <melon9751@naver.com>
2021-04-23 20:23:36 +0900

    인터뷰 목록 조회 요청에 페이징을 추가하라
    인터뷰 목록 조회 요청에 페이징을 추가합니다.
    
    해당 페이지의 인터뷰 목록을 불러오는 서비스를 추가해야 합니다.

--- app/src/main/java/com/example/bookclub/controllers/InterviewController.java
+++ app/src/main/java/com/example/bookclub/controllers/InterviewController.java
@@ -4,10 +4,12 @@ import com.example.bookclub.application.InterviewService;
 import com.example.bookclub.domain.Account;
 import com.example.bookclub.domain.Interview;
 import com.example.bookclub.security.CurrentAccount;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -15,6 +17,11 @@ import java.util.List;
 @RequestMapping("/interviews")
 public class InterviewController {
     private final InterviewService interviewService;
+    @Value("${countList}")
+    private int countList;
+
+    @Value("${countPage}")
+    private int countPage;
 
     public InterviewController(InterviewService interviewService) {
         this.interviewService = interviewService;
@@ -22,12 +29,31 @@ public class InterviewController {
 
     @GetMapping
     public String interviewLists(@CurrentAccount Account account,
+                                 @RequestParam String targetPage,
                                  Model model) {
         if(account != null) {
             checkTopMenu(account, model);
         }
         List<Interview> lists = interviewService.getInterviews();
-        model.addAttribute("interviews", lists);
+//        model.addAttribute("interviews", lists);
+
+        int allInterviewsCount = lists.size();
+        int totalPage = allInterviewsCount / countList;
+        if(allInterviewsCount % countList > 0) {
+            totalPage += 1;
+        }
+        int nowPage = Integer.parseInt(targetPage);
+        if(nowPage > totalPage) {
+            nowPage = totalPage;
+        }
+        int firstPage = ((nowPage - 1) / countPage) * countPage + 1;
+        int lastPage = firstPage + countPage - 1;
+        if(lastPage > totalPage) {
+            lastPage = totalPage;
+        }
+        List<Interview> interviews = interviewService
+                .getNowPageInterviews((nowPage - 1) * countList, countList);
+        
         //List<Interview> list = interviewService.crawlAllInterviews();
         return "interviews/interviews-list";
     }
