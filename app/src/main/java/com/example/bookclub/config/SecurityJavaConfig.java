5f9f5b11df4c65462e3a4b7654db71a60fd5d562
sunggyupaik <melon9751@naver.com>
2021-11-03 00:42:28 +0900

    로그인 시큐리티를 개선하라
    로그인 시큐리티로 로그인, 로그아웃 설정을 하며 세션갯수를 제한합니다.
    세션을 이용하기 위해 세션관련 설정도 추가합니다

--- app/src/main/java/com/example/bookclub/config/SecurityJavaConfig.java
+++ app/src/main/java/com/example/bookclub/config/SecurityJavaConfig.java
@@ -1,32 +1,65 @@
 package com.example.bookclub.config;
 
-import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configurers.SessionManagementConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.session.SessionRegistry;
+import org.springframework.security.core.session.SessionRegistryImpl;
+import org.springframework.security.web.session.HttpSessionEventPublisher;
 
-@Configuration
+import javax.servlet.http.HttpSessionEvent;
+import java.time.LocalDateTime;
+
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityJavaConfig extends WebSecurityConfigurerAdapter {
+    @Bean
+    public ServletListenerRegistrationBean<HttpSessionEventPublisher> httpSessionEventPublisher() {
+        return new ServletListenerRegistrationBean<HttpSessionEventPublisher>(new HttpSessionEventPublisher(){
+            @Override
+            public void sessionCreated(HttpSessionEvent event) {
+                super.sessionCreated(event);
+                System.out.printf("===>> [%s] 세션 생성됨 %s \n", LocalDateTime.now(), event.getSession().getId());
+            }
+
+            @Override
+            public void sessionDestroyed(HttpSessionEvent event) {
+                super.sessionDestroyed(event);
+                System.out.printf("===>> [%s] 세션 만료됨 %s \n", LocalDateTime.now(), event.getSession().getId());
+            }
+        });
+    }
+
+    @Bean
+    SessionRegistry sessionRegistry(){
+        return new SessionRegistryImpl();
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
                 .anyRequest().permitAll()
         .and()
-                .formLogin()
-                .loginPage("/login")
-                .loginProcessingUrl("/")
-                .defaultSuccessUrl("/", false)
-                .permitAll()
-        .and()
-                .logout()
-                .logoutUrl("/logout")
-        .and()
-                .sessionManagement(s-> s
-                                .sessionFixation(SessionManagementConfigurer.SessionFixationConfigurer::changeSessionId)
-                                .maximumSessions(1));
+                .formLogin(login ->
+                        login.loginPage("/login")
+                        .loginProcessingUrl("/login")
+                        .permitAll()
+                        .defaultSuccessUrl("/", false)
+                        .failureUrl("/login-error")
+                )
+                .logout(logout ->
+                        logout.logoutSuccessUrl("/")
+                )
+                .sessionManagement(s->
+                        s.sessionFixation(SessionManagementConfigurer.SessionFixationConfigurer::changeSessionId)
+                        .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
+                        .maximumSessions(1)
+                        .maxSessionsPreventsLogin(true)
+                );
 
         http
                 .csrf().disable()
