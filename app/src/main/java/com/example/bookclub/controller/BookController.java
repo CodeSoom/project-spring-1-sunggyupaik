dbd512ea4c84ddd4e82ad5dd91d96a82b1f8775a
sunggyupaik <melon9751@naver.com>
2021-11-15 19:31:16 +0900

    책 검색 요청을 개선하라
    책 검색 요청의 중복 부분을 리팩토링 합니다

--- app/src/main/java/com/example/bookclub/controller/BookController.java
+++ app/src/main/java/com/example/bookclub/controller/BookController.java
@@ -6,14 +6,11 @@ import com.example.bookclub.domain.Account;
 import com.example.bookclub.domain.BookType;
 import com.example.bookclub.security.CurrentAccount;
 import org.json.simple.JSONArray;
-import org.json.simple.parser.ParseException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import java.io.IOException;
 
 @Controller
 @RequestMapping("/books")
@@ -28,48 +25,38 @@ public class BookController {
     }
 
     @GetMapping("/bestseller")
-    public String booksBestSellerLists(@CurrentAccount Account account, Model model)
-            throws IOException, ParseException {
+    public String booksBestSellerLists(@CurrentAccount Account account, Model model) {
         checkTopMenu(account, model);
 
-        JSONArray bestSellers = bookService.getBookLists(BookType.BESTSELLER, "");
-        model.addAttribute("book", bestSellers);
-        model.addAttribute("bookType", BookType.getTitleFrom(BookType.BESTSELLER));
-        return "books/books-lists";
+        return getBookList(model, BookType.BESTSELLER, "");
     }
 
     @GetMapping("/recommend")
-    public String booksRecommendLists(@CurrentAccount Account account, Model model)
-            throws IOException, ParseException {
+    public String booksRecommendLists(@CurrentAccount Account account, Model model) {
         checkTopMenu(account, model);
 
-
-        JSONArray recommends = bookService.getBookLists(BookType.RECOMMEND, "");
-        model.addAttribute("book", recommends);
-        model.addAttribute("bookType", BookType.getTitleFrom(BookType.RECOMMEND));
-        return "books/books-lists";
+        return getBookList(model, BookType.RECOMMEND, "");
     }
 
     @GetMapping("/new")
-    public String booksNewLists(@CurrentAccount Account account, Model model)
-            throws IOException, ParseException {
+    public String booksNewLists(@CurrentAccount Account account, Model model) {
         checkTopMenu(account, model);
 
-        JSONArray newBooks = bookService.getBookLists(BookType.NEW, "");
-        model.addAttribute("book", newBooks);
-        model.addAttribute("bookType", BookType.getTitleFrom(BookType.NEW));
-        return "books/books-lists";
+        return getBookList(model, BookType.NEW, "");
     }
 
     @GetMapping("/search")
     public String booksSearchLists(@CurrentAccount Account account,
-                                   Model model, @RequestParam String keyword)
-            throws IOException, ParseException {
+                                   Model model, @RequestParam String keyword) {
         checkTopMenu(account, model);
+        
+        return getBookList(model, BookType.SEARCH, keyword);
+    }
 
-        JSONArray searchBooks = bookService.getBookLists(BookType.SEARCH, keyword);
+    private String getBookList(Model model, BookType bookType, String keyword) {
+        JSONArray searchBooks = bookService.getBookLists(bookType, keyword);
         model.addAttribute("book", searchBooks);
-        model.addAttribute("bookType", BookType.getTitleFrom(BookType.SEARCH));
+        model.addAttribute("bookType", BookType.getTitleFrom(bookType));
         return "books/books-lists";
     }
 
