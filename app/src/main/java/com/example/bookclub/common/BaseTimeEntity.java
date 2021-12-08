d4dd16ce3cd6a73fcab3c69f21cd950e574bb5ec
sunggyupaik <melon9751@naver.com>
2021-12-09 07:57:08 +0900

    시간, 작성자 자동 기록 감지를 분리하라
    시간기록이 필요한 경우와 작성자 기록이 필요한 경우가 각각 있을 수 있기
    때문에, 2개를 분리하도록 개선합니다

--- app/src/main/java/com/example/bookclub/common/BaseTimeEntity.java
+++ app/src/main/java/com/example/bookclub/common/BaseTimeEntity.java
@@ -1,22 +1,23 @@
 package com.example.bookclub.common;
 
 import lombok.Getter;
-import org.springframework.data.annotation.CreatedBy;
-import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.Column;
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
 
 @EntityListeners(value = AuditingEntityListener.class)
 @MappedSuperclass
 @Getter
-public abstract class BaseEntity extends BaseTimeEntity {
-	@CreatedBy
+public class BaseTimeEntity {
+	@CreatedDate
 	@Column(updatable = false)
-	private String createdBy;
+	private LocalDateTime createdDate;
 
-	@LastModifiedBy
-	private String updatedBy;
+	@LastModifiedDate
+	private LocalDateTime updatedDate;
 }
