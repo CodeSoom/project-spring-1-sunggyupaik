c10f2637bfb3a927b01b3e29fb2582a1d2181d8c
sunggyupaik <melon9751@naver.com>
2022-01-24 08:07:25 +0900

    스터디 댓글 좋아요 도메인을 추가하라
    스터디 댓글 좋아요 도메인을 추가합니다. 연관관계가 있는 계정과 스터디
    댓글 도메인에도 연관관계를 추가해야 합니다

---
+++ app/src/main/java/com/example/bookclub/domain/StudyCommentLike.java
@@ -0,0 +1,44 @@
+package com.example.bookclub.domain;
+
+import com.example.bookclub.common.BaseTimeEntity;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString
+public class StudyCommentLike extends BaseTimeEntity {
+	@Id
+	@GeneratedValue
+	@Column(name = "STUDYCOMMENTLIKE_ID")
+	private Long id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "ACCOUNT_ID")
+	@ToString.Exclude
+	private Account account;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "COMMENT_ID")
+	@ToString.Exclude
+	private StudyComment studyComment;
+
+	@Builder
+	public StudyCommentLike(Long id, Account account, StudyComment studyComment) {
+		this.id = id;
+		this.account = account;
+		this.studyComment = studyComment;
+	}
+}
