8708b0dbdd0de1b6b2383b67785d7f3006775fae
sunggyupaik <melon9751@naver.com>
2022-02-26 10:21:27 +0900

    모든 인터뷰조회 반환형을 개선하라
    querydsl을 사용하여 한번에 반환형을 dto로 조회하도록 개선하기 위해
    도메인 대신 dto를 사용합니다

--- app/src/main/java/com/example/bookclub/repository/interview/InterviewRepositoryCustom.java
+++ app/src/main/java/com/example/bookclub/repository/interview/InterviewRepositoryCustom.java
@@ -1,13 +1,14 @@
 package com.example.bookclub.repository.interview;
 
 import com.example.bookclub.domain.Interview;
+import com.example.bookclub.dto.InterviewResultDto;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 import java.util.Optional;
 
 public interface InterviewRepositoryCustom {
-	Page<Interview> findAll(Pageable pageable);
+	Page<InterviewResultDto> findAll(Pageable pageable);
 
 	Optional<Interview> findByTitle(String title);
 }
