dac04a3ca97f7c7292a3ddc8d609b9bf575f48f8
sunggyupaik <melon9751@naver.com>
2022-02-26 10:27:03 +0900

    인터뷰 전체조회 퀴리를 개선하라
    기존에 저장소에서 바로 꺼내오던 것을 querydsl을 이용해 바로 dto로
    조회하도록 개선합니다

--- app/src/main/java/com/example/bookclub/repository/interview/JpaInterviewRepositoryImpl.java
+++ app/src/main/java/com/example/bookclub/repository/interview/JpaInterviewRepositoryImpl.java
@@ -2,24 +2,48 @@ package com.example.bookclub.repository.interview;
 
 import com.example.bookclub.domain.Interview;
 import com.example.bookclub.dto.InterviewResultDto;
+import com.example.bookclub.dto.QInterviewResultDto;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
+
+import static com.example.bookclub.domain.QInterview.interview;
 
 @Repository
 public class JpaInterviewRepositoryImpl implements InterviewRepositoryCustom {
-	private final JPAQueryFactory jpaQueryFactory;
+	private final JPAQueryFactory queryFactory;
 
-	public JpaInterviewRepositoryImpl(JPAQueryFactory jpaQueryFactory) {
-		this.jpaQueryFactory = jpaQueryFactory;
+	public JpaInterviewRepositoryImpl(JPAQueryFactory queryFactory) {
+		this.queryFactory = queryFactory;
 	}
 
 	@Override
 	public Page<InterviewResultDto> findAll(Pageable pageable) {
-		return null;
+		List<InterviewResultDto> content = queryFactory
+				.select(new QInterviewResultDto(
+						interview.interviewUrl,
+						interview.imgUrl,
+						interview.author,
+						interview.title,
+						interview.date,
+						interview.content
+				))
+				.from(interview)
+				.offset(pageable.getOffset())
+				.limit(pageable.getPageSize())
+				.fetch();
+
+		long total = queryFactory
+				.select(interview)
+				.from(interview)
+				.fetchCount();
+
+		return new PageImpl<>(content, pageable, total);
 	}
 
 	@Override
