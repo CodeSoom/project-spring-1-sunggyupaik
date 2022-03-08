a13bc233036c2bd7c26cf687337cafb5778cc028
sunggyupaik <melon9751@naver.com>
2022-03-09 08:31:16 +0900

    스터디 좋아요 서비스에 javadoc을 추가하라
    스터디 좋아요 서비스에 javadoc을 추가합니다

--- app/src/main/java/com/example/bookclub/application/StudyLikeService.java
+++ app/src/main/java/com/example/bookclub/application/StudyLikeService.java
@@ -11,6 +11,10 @@ import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 
+/**
+ * 스터디 좋아요 생성, 삭제를 한다.
+ *
+ */
 @Service
 public class StudyLikeService {
 	private final StudyService studyService;
@@ -25,6 +29,14 @@ public class StudyLikeService {
 		this.studyLikeRepository = studyLikeRepository;
 	}
 
+	/**
+	 * 로그인한 사용자와 스터디 아이디로 스터디 좋아요를 생성하고 아이디를 반환한다.
+	 *
+	 * @param userAccount 로그인한 사용자
+	 * @param studyId 스터디 아이디 식별자
+	 * @return 생성된 스터디 좋아요 아이디
+	 * @throws StudyLikeAlreadyExistedException 주어진 스터디 아이디에 해당하는 좋아요가 존재하는 경우
+	 */
 	@Transactional
 	public Long like(UserAccount userAccount, Long studyId) {
 		Long accountId = userAccount.getAccount().getId();
@@ -47,6 +59,14 @@ public class StudyLikeService {
 		return studyId;
 	}
 
+	/**
+	 * 로그인한 사용자와 스터디 아이디로 스터디 좋아요를 제거하고 아이디를 반환한다.
+	 *
+	 * @param userAccount 로그인한 사용자
+	 * @param studyId 스터디 아이디 식별자
+	 * @return 삭제된 스터디 좋아요 아이디
+	 * @throws StudyLikeNotExistedException 주어진 스터디 아이디에 해당하는 좋아요가 없는 경우
+	 */
 	@Transactional
 	public Long unLike(UserAccount userAccount, Long studyId) {
 		Long accountId = userAccount.getAccount().getId();
