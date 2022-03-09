fb91d6031dbf40c156244378de0ff23f9ebd81ad
sunggyupaik <melon9751@naver.com>
2022-03-09 13:36:11 +0900

    파일 업로드에 javadoc을 추가하라
    파일 업로드에 javadoc을 추가합니다

--- app/src/main/java/com/example/bookclub/domain/UploadFile.java
+++ app/src/main/java/com/example/bookclub/domain/UploadFile.java
@@ -13,20 +13,28 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
 
+/**
+ * 업로드 파일
+ */
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class UploadFile extends BaseTimeEntity {
+    /* 식별자 */
     @Id @GeneratedValue
     @Column(name = "UPLOADFILE_ID")
     private Long id;
 
+    /* 파일명 */
     private String fileName;
 
+    /* 파일 원본명 */
     private String fileOriginalName;
 
+    /* 파일 URL */
     private String fileUrl;
 
+    /* 사용자 */
     @OneToOne(mappedBy = "uploadFile")
     @ToString.Exclude
     private Account account;
