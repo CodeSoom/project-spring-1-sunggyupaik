7d590a7be2532c1db2d158725c65cb3c431b0bc3
sunggyupaik <melon9751@naver.com>
2022-03-09 11:39:35 +0900

    인터뷰 도메인에 javadoc을 추가하라
    인터뷰 ㄷ메인에 javadoc을 추가합니다

--- app/src/main/java/com/example/bookclub/domain/Interview.java
+++ app/src/main/java/com/example/bookclub/domain/Interview.java
@@ -13,25 +13,35 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import java.time.LocalDate;
 
+/**
+ * 인터뷰
+ */
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @ToString
 public class Interview extends BaseTimeEntity {
+    /* 식별자 */
     @Id @GeneratedValue
     @Column(name = "INTERVIEW_ID")
     private Long id;
 
+    /* 인터뷰 URL */
     private String interviewUrl;
 
+    /* 이미지 URL */
     private String imgUrl;
 
+    /* 작가 */
     private String author;
 
+    /* 제목 */
     private String title;
 
+    /* 날짜 */
     private LocalDate date;
 
+    /* 내용 */
     @Column(length = 1000)
     private String content;
 
