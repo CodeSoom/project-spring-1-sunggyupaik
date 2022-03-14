57f4f815995d78fa8b541c874fe87c584be00e3d
sunggyupaik <melon9751@naver.com>
2022-03-14 21:25:55 +0900

    이미 존재하는 스터디 댓글 좋아요 생성 서비스 테스트를 추가하라
    이미 존재하는 스터디 댓글 좋아요를 생성하려고 하면 이미 좋아요가
    존재한다는 메세지를 반환하는 테스트를 추가합니다

--- app/src/test/java/com/example/bookclub/application/StudyCommentLikeServiceTest.java
+++ app/src/test/java/com/example/bookclub/application/StudyCommentLikeServiceTest.java
@@ -5,6 +5,7 @@ import com.example.bookclub.domain.Study;
 import com.example.bookclub.domain.StudyComment;
 import com.example.bookclub.domain.StudyCommentLike;
 import com.example.bookclub.domain.StudyCommentLikeRepository;
+import com.example.bookclub.errors.StudyCommentLikeAlreadyExistedException;
 import com.example.bookclub.errors.StudyCommentNotFoundException;
 import com.example.bookclub.security.UserAccount;
 import org.junit.jupiter.api.BeforeEach;
@@ -14,10 +15,12 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
 
@@ -77,7 +80,7 @@ class StudyCommentLikeServiceTest {
 	}
 
 	@Test
-	void likeCommentWithExistedStudyCommentId() {
+	void createLikeCommentWithExistedStudyCommentId() {
 		given(studyCommentLikeRepository.save(any(StudyCommentLike.class))).willReturn(createdStudyCommentLike);
 
 		studyCommentLikeRepository.save(createdStudyCommentLike);
@@ -86,7 +89,7 @@ class StudyCommentLikeServiceTest {
 	}
 
 	@Test
-	void likeCommentWithNotExistedStudyCommentId() {
+	void createLikeCommentWithNotExistedStudyCommentId() {
 		given(studyCommentService.getStudyComment(STUDY_COMMENT_NOT_EXISTED_ID))
 				.willThrow(StudyCommentNotFoundException.class);
 
@@ -94,5 +97,18 @@ class StudyCommentLikeServiceTest {
 				() -> studyCommentLikeService.likeComment(userAccount, STUDY_COMMENT_NOT_EXISTED_ID)
 		)
 				.isInstanceOf(StudyCommentNotFoundException.class);
+	}
+
+	@Test
+	void createLikeCommentWithAlreadyExisted() {
+		given(studyCommentService.getStudyComment(STUDY_COMMENT_EXISTED_ID)).willReturn(setUpStudyComment);
+		given(accountService.findAccount(ACCOUNT_EXISTED_ID)).willReturn(account);
+		given(studyCommentLikeRepository.findByStudyCommentAndAccount(eq(setUpStudyComment), eq(account)))
+				.willReturn(Optional.of(createdStudyCommentLike));
+
+		assertThatThrownBy(
+				() -> studyCommentLikeService.likeComment(userAccount, STUDY_COMMENT_EXISTED_ID)
+		)
+				.isInstanceOf(StudyCommentLikeAlreadyExistedException.class);
 	}
 }
