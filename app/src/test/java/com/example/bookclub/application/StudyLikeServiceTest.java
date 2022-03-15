047567273d18f95e8a39e6730579cd89e7f37cde
sunggyupaik <melon9751@naver.com>
2022-03-16 07:28:57 +0900

    존재하지 않는 스터디의 좋아요 생성 서비스 테스트를 추가하라
    조재하지 않는 스터디 식별자에 조하요 생성을 하면 스터디가 존재하지
    않는다는 메세지를 반환하는 테스트를 추가하니다

--- app/src/test/java/com/example/bookclub/application/StudyLikeServiceTest.java
+++ app/src/test/java/com/example/bookclub/application/StudyLikeServiceTest.java
@@ -4,6 +4,7 @@ import com.example.bookclub.domain.Account;
 import com.example.bookclub.domain.Study;
 import com.example.bookclub.domain.StudyLike;
 import com.example.bookclub.domain.StudyLikeRepository;
+import com.example.bookclub.errors.StudyNotFoundException;
 import com.example.bookclub.security.UserAccount;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -14,6 +15,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
@@ -77,5 +79,16 @@ public class StudyLikeServiceTest {
 		Long createdStudyLikeId = studyLikeService.like(userAccount, STUDY_EXISTED_ID);
 
 		assertThat(createdStudyLikeId).isEqualTo(STUDY_EXISTED_ID);
+	}
+
+	@Test
+	void createStudyLikeWithNotExistedStudyId() {
+		given(studyService.getStudy(STUDY_NOT_EXISTED_ID)).willThrow(StudyNotFoundException.class);
+		given(accountService.findAccount(ACCOUNT_ID)).willReturn(account);
+
+		assertThatThrownBy(
+				() -> studyLikeService.like(userAccount, STUDY_NOT_EXISTED_ID)
+		)
+				.isInstanceOf(StudyNotFoundException.class);
 	}
 }
