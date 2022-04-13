865d0d9feacf352acd883ed9ebfd6ac17f3dc89a
sunggyupaik <melon9751@naver.com>
2022-04-13 23:43:10 +0900

    인터뷰 크롤링 요청 테스트에 rest docs를 추가하라
    인터뷰 크롤링 요청 테스트에 rest docs를 추가합니다

--- app/src/test/java/com/example/bookclub/controller/api/InterviewApiControllerTest.java
+++ app/src/test/java/com/example/bookclub/controller/api/InterviewApiControllerTest.java
@@ -11,10 +11,15 @@ import com.example.bookclub.security.UserAccount;
 import org.hamcrest.core.StringContains;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -26,16 +31,27 @@ import org.springframework.web.filter.CharacterEncodingFilter;
 
 import javax.sql.DataSource;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
+import static com.example.bookclub.utils.ApiDocumentUtils.getDocumentRequest;
+import static com.example.bookclub.utils.ApiDocumentUtils.getDocumentResponse;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.BDDMockito.given;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.restdocs.payload.JsonFieldType.NUMBER;
+import static org.springframework.restdocs.payload.JsonFieldType.STRING;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(InterviewApiController.class)
+@AutoConfigureRestDocs(uriScheme = "https", uriHost = "docs.api.com")
+@ExtendWith({RestDocumentationExtension.class})
 class InterviewApiControllerTest {
 	private final static Long FIRST_ID = 1L;
 	private final static String FIRST_INTERVIEW_URL = "firstInterviewUrl";
@@ -44,6 +60,8 @@ class InterviewApiControllerTest {
 	private final static String FIRST_TITLE = "firstTitle";
 	private final static LocalDate FIRST_DATE = LocalDate.now();
 	private final static String FIRST_CONTENT = "firstContent";
+	private final static LocalDateTime FIRST_CREATED_DATE = LocalDateTime.now();
+	private final static LocalDateTime FIRST_UPDATED_DATE = LocalDateTime.now();
 
 	private final static Long SECOND_ID = 2L;
 	private final static String SECOND_INTERVIEW_URL = "secondInterviewUrl";
@@ -52,6 +70,8 @@ class InterviewApiControllerTest {
 	private final static String SECOND_TITLE = "secondTitle";
 	private final static LocalDate SECOND_DATE = LocalDate.now();
 	private final static String SECOND_CONTENT = "secondContent";
+	private final static LocalDateTime SECOND_CREATED_DATE = LocalDateTime.now();
+	private final static LocalDateTime SECOND_UPDATED_DATE = LocalDateTime.now();
 
 	private static final Long ACCOUNT_ID = 3L;
 	private static final String ACCOUNT_NAME = "accountName";
@@ -93,9 +113,10 @@ class InterviewApiControllerTest {
 	private UsernamePasswordAuthenticationToken userAccountToken;
 
 	@BeforeEach
-	void setUp() {
+	void setUp(RestDocumentationContextProvider restDocumentationContextProvider) {
 		this.mockMvc = MockMvcBuilders.webAppContextSetup(ctx)
 				.addFilters(new CharacterEncodingFilter("UTF-8", true))
+				.apply(documentationConfiguration(restDocumentationContextProvider))
 				.alwaysDo(print())
 				.build();
 
@@ -109,6 +130,9 @@ class InterviewApiControllerTest {
 				.content(FIRST_CONTENT)
 				.build();
 
+		firstInterview.setCreatedDate(FIRST_CREATED_DATE);
+		firstInterview.setUpdatedDate(FIRST_UPDATED_DATE);
+
 		secondInterview = Interview.builder()
 				.id(SECOND_ID)
 				.interviewUrl(SECOND_INTERVIEW_URL)
@@ -118,6 +142,9 @@ class InterviewApiControllerTest {
 				.date(SECOND_DATE)
 				.content(SECOND_CONTENT)
 				.build();
+
+		secondInterview.setCreatedDate(SECOND_CREATED_DATE);
+		secondInterview.setUpdatedDate(SECOND_UPDATED_DATE);
 
 		adminAccount = Account.builder()
 				.id(ACCOUNT_ID)
@@ -153,14 +180,29 @@ class InterviewApiControllerTest {
 		SecurityContextHolder.getContext().setAuthentication(adminAccountToken);
 		given(interviewService.crawlAllInterviews()).willReturn((interviewList));
 
-		mockMvc.perform(
-				post("/api/interviews")
+		this.mockMvc.perform(
+						RestDocumentationRequestBuilders.post("/api/interviews")
 						.contentType(MediaType.APPLICATION_JSON)
 		)
 				.andDo(print())
 				.andExpect(status().isCreated())
 				.andExpect(content().string(StringContains.containsString("\"imgUrl\":\"" + FIRST_IMG_URL)))
-				.andExpect(content().string(StringContains.containsString("\"imgUrl\":\"" + SECOND_IMG_URL)));
+				.andExpect(content().string(StringContains.containsString("\"imgUrl\":\"" + SECOND_IMG_URL)))
+				.andDo(document("interview-create",
+						getDocumentRequest(),
+						getDocumentResponse(),
+						responseFields(
+								fieldWithPath("[].id").type(NUMBER).description("인터뷰 식별자")	,
+								fieldWithPath("[].interviewUrl").type(STRING).description("인터뷰 URL"),
+								fieldWithPath("[].imgUrl").type(STRING).description("이미지 URL")	,
+								fieldWithPath("[].author").type(STRING).description("작가")	,
+								fieldWithPath("[].title").type(STRING).description("제목"),
+								fieldWithPath("[].date").type(STRING).description("날짜"),
+								fieldWithPath("[].content").type(STRING).description("내용"),
+								fieldWithPath("[].createdDate").type(STRING).description("생성날짜"),
+								fieldWithPath("[].updatedDate").type(STRING).description("수정날짜")
+						)
+				));
 	}
 
 	@Test
