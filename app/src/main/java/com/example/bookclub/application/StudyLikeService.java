40fe609dd55310379dd226685df26982e6a5adab
sunggyupaik <melon9751@naver.com>
2022-04-15 22:54:17 +0900

    스터디 요청에 주석을 추가하라
    스터디 요청에 주석을 추가합니다

--- app/src/main/java/com/example/bookclub/application/StudyLikeService.java
+++ app/src/main/java/com/example/bookclub/application/StudyLikeService.java
@@ -31,12 +31,12 @@ public class StudyLikeService {
 	}
 
 	/**
-	 * 로그인한 사용자와 스터디 아이디로 스터디 좋아요를 생성하고 아이디를 반환한다.
+	 * 주어진 로그인한 사용자와 스터디 식별자에 해당하는 스터디 좋아요를 생성하고 스터디 식별자를 반환한다
 	 *
 	 * @param userAccount 로그인한 사용자
-	 * @param studyId 스터디 아이디 식별자
+	 * @param studyId 스터디 식별자
 	 * @return 생성된 스터디 좋아요 아이디
-	 * @throws StudyLikeAlreadyExistedException 주어진 스터디 아이디에 해당하는 좋아요가 존재하는 경우
+	 * @throws StudyLikeAlreadyExistedException 주어진 스터디 식별자에 해당하는 좋아요가 존재하는 경우
 	 */
 	@Transactional
 	public StudyLikeResultDto like(UserAccount userAccount, Long studyId) {
@@ -61,12 +61,12 @@ public class StudyLikeService {
 	}
 
 	/**
-	 * 로그인한 사용자와 스터디 아이디로 스터디 좋아요를 제거하고 아이디를 반환한다.
+	 * 주어진 로그인한 사용자와 스터디 식별자에 해당하는 스터디 좋아요를 삭제하고 스터디 식별자를 반환한다.
 	 *
 	 * @param userAccount 로그인한 사용자
-	 * @param studyId 스터디 아이디 식별자
-	 * @return 삭제된 스터디 좋아요 아이디
-	 * @throws StudyLikeNotExistedException 주어진 스터디 아이디에 해당하는 좋아요가 없는 경우
+	 * @param studyId 스터디 식별자
+	 * @return 삭제된 스터디 좋아요 식별자
+	 * @throws StudyLikeNotExistedException 주어진 스터디 식별자에 해당하는 좋아요가 없는 경우
 	 */
 	@Transactional
 	public StudyLikeResultDto unLike(UserAccount userAccount, Long studyId) {
