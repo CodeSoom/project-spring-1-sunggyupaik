f28ca64b12bc40b7ef7b81011ee2969e3e75e96b
sunggyupaik <melon9751@naver.com>
2022-05-07 11:11:55 +0900

    카카오 로그인 테스트를 개선하라
    기존에 회원가입되어있는 경우 카카오 로그인 요청 시 해당 계정으로
    로그인하는 테스트를 개선합니다

--- app/src/test/java/com/example/bookclub/controller/api/LoginApiControllerTest.java
+++ app/src/test/java/com/example/bookclub/controller/api/LoginApiControllerTest.java
@@ -1,10 +1,14 @@
 package com.example.bookclub.controller.api;
 
 import com.example.bookclub.application.AccountAuthenticationService;
+import com.example.bookclub.application.EmailService;
+import com.example.bookclub.application.LoginService;
+import com.example.bookclub.domain.Account;
 import com.example.bookclub.dto.KakaoLoginRequest;
 import com.example.bookclub.security.CustomDeniedHandler;
 import com.example.bookclub.security.CustomEntryPoint;
 import com.example.bookclub.security.PersistTokenRepository;
+import com.example.bookclub.security.UserAccount;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -17,24 +21,38 @@ import org.springframework.http.MediaType;
 import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.filter.CharacterEncodingFilter;
 
 import javax.sql.DataSource;
+import java.util.List;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(LoginApiControllerTest.class)
+@WebMvcTest(LoginApiController.class)
 @AutoConfigureRestDocs(uriScheme = "https", uriHost = "docs.api.com")
 @ExtendWith({RestDocumentationExtension.class})
 public class LoginApiControllerTest {
 	private final static String KAKAO_LOGIN_EMAIL = "kakaoLoginEmail";
 
 	private KakaoLoginRequest kakaoLoginRequest;
+	private Account account;
+	private UsernamePasswordAuthenticationToken accountToken;
+
+	@MockBean
+	private LoginService loginService;
+
+	@MockBean
+	private EmailService emailService;
 
 	@MockBean
 	private DataSource dataSource;
@@ -71,16 +89,31 @@ public class LoginApiControllerTest {
 		kakaoLoginRequest = KakaoLoginRequest.builder()
 				.email(KAKAO_LOGIN_EMAIL)
 				.build();
+
+		account = Account.builder()
+				.email(KAKAO_LOGIN_EMAIL)
+				.build();
+
+		accountToken =
+				new UsernamePasswordAuthenticationToken(
+						new UserAccount(account, List.of(new SimpleGrantedAuthority("KAKAO-USER"))),
+						null,
+						List.of(new SimpleGrantedAuthority("KAKAO-USER")));
 	}
 
 	@Test
-	void loginWithKakao() throws Exception {
+	void loginKakaoWithAlreadyExistedEmail() throws Exception {
+		given(loginService.checkAlreadyExistedEmail(any(KakaoLoginRequest.class))).willReturn(true);
+		given(loginService.makeKakaoAuthenticationToken(any(KakaoLoginRequest.class))).willReturn(accountToken);
+
 		mockMvc.perform(
 				RestDocumentationRequestBuilders.post("/api/kakao-login")
 						.contentType(MediaType.APPLICATION_JSON)
 						.content(objectMapper.writeValueAsString(kakaoLoginRequest))
 		)
 				.andDo(print())
-				.andExpect(status().isOk());
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("email").value(KAKAO_LOGIN_EMAIL))
+				.andExpect(jsonPath("authenticationNumber").isEmpty());
 	}
 }
