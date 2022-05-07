f44d12b7d0cafc0774729bae7ac5df7e99d691a8
sunggyupaik <melon9751@naver.com>
2022-05-07 11:55:44 +0900

    회원이 존재하는 카카오 로그인 서비스를 테스트하라
    회원이 존재하는 카카오 로그인 서비스 테스트를 추가합니다

---
+++ app/src/test/java/com/example/bookclub/application/LoginServiceTest.java
@@ -0,0 +1,64 @@
+package com.example.bookclub.application;
+
+import com.example.bookclub.domain.Account;
+import com.example.bookclub.dto.KakaoLoginRequest;
+import com.example.bookclub.repository.account.JpaAccountRepository;
+import com.example.bookclub.security.UserAccount;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+
+class LoginServiceTest {
+	private final static String ACCOUNT_EXISTED_EMAIL = "accountExistedEmail";
+
+	private LoginService loginService;
+	private AccountAuthenticationService accountAuthenticationService;
+	private JpaAccountRepository accountRepository;
+	private UserAccount userAccount;
+	private Account account;
+	private UsernamePasswordAuthenticationToken accountToken;
+	private KakaoLoginRequest kakaoLoginRequest;
+
+	@BeforeEach
+	void setUp() {
+		accountAuthenticationService = mock(AccountAuthenticationService.class);
+		accountRepository = mock(JpaAccountRepository.class);
+		loginService = new LoginService(accountAuthenticationService, accountRepository);
+
+		account = Account.builder()
+				.email(ACCOUNT_EXISTED_EMAIL)
+				.build();
+
+		userAccount = UserAccount.builder()
+				.account(account)
+				.authorities(List.of(new SimpleGrantedAuthority("USER")))
+				.build();
+
+		accountToken = new UsernamePasswordAuthenticationToken(
+				new UserAccount(account, List.of(new SimpleGrantedAuthority("USER"))),
+				account.getPassword(),
+				List.of(new SimpleGrantedAuthority("USER")));
+
+		kakaoLoginRequest = KakaoLoginRequest.builder()
+				.email(ACCOUNT_EXISTED_EMAIL)
+				.build();
+	}
+
+	@Test
+	void checkAlreadyExistedEmailTrue() {
+		given(accountAuthenticationService.loadUserByUsername(eq(ACCOUNT_EXISTED_EMAIL)))
+				.willReturn(userAccount);
+
+		boolean emailExisted = loginService.checkAlreadyExistedEmail(kakaoLoginRequest);
+
+		assertThat(emailExisted).isTrue();
+	}
+}
