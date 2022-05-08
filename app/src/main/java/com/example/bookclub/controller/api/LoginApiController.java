7979516cab3607c797871d2fe9dc6cd525b918e9
sunggyupaik <melon9751@naver.com>
2022-05-08 20:22:38 +0900

    카카오 로그인 요청에 주석을 추가하라
    카카오 로그인 요청에 주석을 추가합니다

--- app/src/main/java/com/example/bookclub/controller/api/LoginApiController.java
+++ app/src/main/java/com/example/bookclub/controller/api/LoginApiController.java
@@ -12,6 +12,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * 카카오 로그인을 요청한다
+ */
 @RestController
 @RequestMapping("/api")
 public class LoginApiController {
@@ -24,6 +27,12 @@ public class LoginApiController {
 		this.emailService = emailService;
 	}
 
+	/**
+	 * 주어진 이메일에 해당하는 사용자가 있으면 해당 사용자를 반환하고 없다면 인증번호를 만들어 반환한다
+	 *
+	 * @param kakaoLoginRequest 카카오 로그인에 성공한 이메일 정보
+	 * @return 생성된 이메일, 인증번호
+	 */
 	@PostMapping("/kakao-login")
 	public EmailSendResultDto kakaoLogin(@RequestBody KakaoLoginRequest kakaoLoginRequest) {
 		if(loginService.checkAlreadyExistedEmail(kakaoLoginRequest)) {
