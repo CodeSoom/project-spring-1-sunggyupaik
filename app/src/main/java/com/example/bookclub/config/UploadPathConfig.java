209aa0629c11fd02412a5fac41a0531aa3e4de2e
sunggyupaik <melon9751@naver.com>
2022-05-16 21:27:40 +0900

    사용하지 않는 파일 업로드 경로를 제거하라
    사용하지 않는 파일 업로드 경로를 주석처리합니다

--- app/src/main/java/com/example/bookclub/config/UploadPathConfig.java
+++ app/src/main/java/com/example/bookclub/config/UploadPathConfig.java
@@ -1,8 +1,6 @@
 package com.example.bookclub.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
@@ -10,14 +8,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  */
 @Configuration
 public class UploadPathConfig implements WebMvcConfigurer {
-    @Value("${resources.uri_path}")
-    private String resourcesUriPath;
-    @Value("${resources.location}")
-    private String resourceLocation;
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler(resourcesUriPath + "/**")
-                .addResourceLocations("file://" + resourceLocation);
-    }
+//    @Value("${resources.uri_path}")
+//    private String resourcesUriPath;
+//    @Value("${resources.location}")
+//    private String resourceLocation;
+//
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        registry.addResourceHandler(resourcesUriPath + "/**")
+//                .addResourceLocations("file://" + resourceLocation);
+//    }
 }
