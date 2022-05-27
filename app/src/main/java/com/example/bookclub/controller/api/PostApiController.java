b130225090e736d2e5e528389fc253d72daab837
sunggyupaik <melon9751@naver.com>
2022-05-27 19:05:05 +0900

    글쓰기 요청을 생성하라
    글쓰기 요청을 생성합니다. 글쓰기 위해 구현하는 producer와 consumer를
    추가로 만들어야 합니다

---
+++ app/src/main/java/com/example/bookclub/controller/api/PostApiController.java
@@ -0,0 +1,31 @@
+package com.example.bookclub.controller.api;
+
+import com.example.bookclub.domain.Post;
+import com.example.bookclub.repository.post.JpaPostRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class PostApiController {
+	private final JpaPostRepository postRepository;
+	private final Producer producer;
+	private final ObjectMapper objectMapper;
+
+	public PostApiController(JpaPostRepository postRepository, Producer producer, ObjectMapper objectMapper) {
+		this.postRepository = postRepository;
+		this.producer = producer;
+		this.objectMapper = objectMapper;
+	}
+
+	@PostMapping("/post")
+	public Post create(@RequestBody Post post) throws JsonProcessingException {
+		String jsonPost = objectMapper.writeValueAsString(post);
+		producer.sendTo(jsonPost);
+		return post;
+	}
+}
