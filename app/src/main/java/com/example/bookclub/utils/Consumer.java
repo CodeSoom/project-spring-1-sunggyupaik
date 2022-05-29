ff603ea3543713dfc17de44bdae3d04653366525
sunggyupaik <melon9751@naver.com>
2022-05-29 19:41:26 +0900

    consumer의 게시글 생성을 개선하라
    기존에 바로 저장소를 이용했던 것을 서비스를 이용하도록 개선합니다

--- app/src/main/java/com/example/bookclub/utils/Consumer.java
+++ app/src/main/java/com/example/bookclub/utils/Consumer.java
@@ -1,7 +1,7 @@
 package com.example.bookclub.utils;
 
+import com.example.bookclub.application.PostService;
 import com.example.bookclub.domain.Post;
-import com.example.bookclub.repository.post.ElasticPostRepository;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
@@ -9,17 +9,17 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Consumer {
-	private final ElasticPostRepository postRepository;
+	private final PostService postService;
 	private final ObjectMapper objectMapper;
 
-	public Consumer(ElasticPostRepository postRepository, ObjectMapper objectMapper) {
-		this.postRepository = postRepository;
+	public Consumer(PostService postService, ObjectMapper objectMapper) {
+		this.postService = postService;
 		this.objectMapper = objectMapper;
 	}
 
 	@RabbitListener(queues = "CREATE_POST_QUEUE")
 	public void handler(String message) throws JsonProcessingException {
 		Post post = objectMapper.readValue(message, Post.class);
-		postRepository.save(post);
+		postService.create(post);
 	}
 }
