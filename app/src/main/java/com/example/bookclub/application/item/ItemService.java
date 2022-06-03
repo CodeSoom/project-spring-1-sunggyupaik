72757967fbabd7efb5ca4489065a8291f339e001
sunggyupaik <melon9751@naver.com>
2022-06-03 13:06:46 +0900

    상품 서비스를 추가하라
    상품 서비스를 추가합니다

---
+++ app/src/main/java/com/example/bookclub/application/item/ItemService.java
@@ -0,0 +1,14 @@
+package com.example.bookclub.application.item;
+
+import com.example.bookclub.domain.Item.Item;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+@Transactional
+public class ItemService {
+	public Item detailItem() {
+		return null;
+	}
+}
