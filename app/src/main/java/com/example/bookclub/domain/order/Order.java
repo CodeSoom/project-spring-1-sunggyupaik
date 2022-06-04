fb198d54504bd137a87b844b7f11223443e56cab
sunggyupaik <melon9751@naver.com>
2022-06-04 20:46:37 +0900

    OrderItem 도메인을 추가하라
    Order와 1:N관계를 가지는 OrderItem 도메인을 추가하고 연관관계를
    설정합니다. OrderItem은 구체적으로 주문의 각 상품과 갯수, 가격을
    나타냅니다

--- app/src/main/java/com/example/bookclub/domain/order/Order.java
+++ app/src/main/java/com/example/bookclub/domain/order/Order.java
@@ -2,6 +2,7 @@ package com.example.bookclub.domain.order;
 
 import com.example.bookclub.common.AccountEntityListener;
 import com.example.bookclub.common.BaseEntity;
+import com.example.bookclub.domain.order.orderitem.OrderItem;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -11,8 +12,12 @@ import lombok.ToString;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EntityListeners;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -31,11 +36,18 @@ public class Order extends BaseEntity {
 
 	private OrderStatus orderStatus;
 
+	@OneToMany(mappedBy = "order", fetch = FetchType.LAZY)
+	@Builder.Default
+	@ToString.Exclude
+	List<OrderItem> orderItems = new ArrayList<>();
+
 	@Builder
-	public Order(Long id, Long accountId, String payMethod, OrderStatus orderStatus) {
+	public Order(Long id, Long accountId, String payMethod, OrderStatus orderStatus,
+				 List<OrderItem> orderItems) {
 		this.id = id;
 		this.accountId = accountId;
 		this.payMethod = payMethod;
 		this.orderStatus = orderStatus;
+		this.orderItems = orderItems;
 	}
 }
