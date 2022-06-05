bf2f371e89c5f2ce5e1bf8d230accfd50acc3d03
sunggyupaik <melon9751@naver.com>
2022-06-05 09:09:53 +0900

    주문 aggregate 생성을 factory로 개선하라
    주문 도메인 이외에 관련된 aggregate의 생성을 factory를 사용하여 서비스
    수준의 코드를 간결하게 유지하도록 개선합니다

--- app/src/main/java/com/example/bookclub/infrastructure/interview/JpaInterviewRepositoryImpl.java
+++ app/src/main/java/com/example/bookclub/infrastructure/interview/JpaInterviewRepositoryImpl.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-import static com.example.bookclub.domain.QInterview.interview;
+import static com.example.bookclub.domain.interview.QInterview.interview;
 import static org.apache.commons.lang3.ObjectUtils.isEmpty;
 
 @Repository
---
+++ app/src/main/java/com/example/bookclub/infrastructure/order/OrderSeriesFactoryImpl.java
@@ -0,0 +1,58 @@
+package com.example.bookclub.infrastructure.order;
+
+import com.example.bookclub.domain.order.Order;
+import com.example.bookclub.domain.order.OrderSeriesFactory;
+import com.example.bookclub.domain.order.item.OrderItem;
+import com.example.bookclub.domain.order.item.OrderItemOption;
+import com.example.bookclub.domain.order.item.OrderItemOptionGroup;
+import com.example.bookclub.dto.OrderDto;
+import com.example.bookclub.infrastructure.order.orderitem.JpaOrderItemRepository;
+import com.example.bookclub.infrastructure.order.orderitemgroup.JpaOrderItemOptionGroupRepository;
+import com.example.bookclub.infrastructure.order.orderitemoption.JpaOrderItemOptionRepository;
+import org.springframework.stereotype.Component;
+
+@Component
+public class OrderSeriesFactoryImpl implements OrderSeriesFactory {
+	private final JpaOrderItemRepository orderItemRepository;
+	private final JpaOrderItemOptionGroupRepository orderItemGroupRepository;
+	private final JpaOrderItemOptionRepository orderItemOptionRepository;
+
+	public OrderSeriesFactoryImpl(JpaOrderItemRepository orderItemRepository,
+								  JpaOrderItemOptionGroupRepository orderItemGroupRepository,
+								  JpaOrderItemOptionRepository orderItemOptionRepository) {
+		this.orderItemRepository = orderItemRepository;
+		this.orderItemGroupRepository = orderItemGroupRepository;
+		this.orderItemOptionRepository = orderItemOptionRepository;
+	}
+
+	@Override
+	public OrderDto.OrderCreateResponse createOrder(Order order, OrderDto.OrderCreateRequest orderCreateRequest) {
+		return (OrderDto.OrderCreateResponse) orderCreateRequest.getOrderItemCreateRequests().stream().map(orderItemCreateRequest -> {
+			OrderItem orderItem = orderItemCreateRequest.toEntity();
+			orderItem.setOrder(order);
+			orderItemRepository.save(orderItem);
+
+			orderItemCreateRequest.getOrderItemOptionGroupCreateRequests().forEach(
+					orderItemOptionGroupCreateRequest -> {
+						OrderItemOptionGroup orderItemOptionGroup = orderItemOptionGroupCreateRequest.toEntity();
+						orderItemOptionGroup.setOrderItem(orderItem);
+						orderItemGroupRepository.save(orderItemOptionGroup);
+
+						orderItemOptionGroupCreateRequest.getOrderItemOptionCreateRequests().forEach(
+								orderItemOptionCreateRequest -> {
+									OrderItemOption orderItemOption = orderItemOptionCreateRequest.toEntity();
+									orderItemOption.setOrderItemOptionGroup(orderItemOptionGroup);
+									orderItemOptionRepository.save(orderItemOption);
+								}
+						);
+					}
+			);
+
+			return OrderDto.OrderCreateResponse.builder()
+					.id(order.getId())
+					.accountId(orderCreateRequest.getAccountId())
+					.payMethod(orderCreateRequest.getPayMethod())
+					.build();
+		});
+	}
+}
