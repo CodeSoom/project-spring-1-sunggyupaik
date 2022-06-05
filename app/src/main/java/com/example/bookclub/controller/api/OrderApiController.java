6b0ee762d86273268d89798deede26f7e0cbc053
sunggyupaik <melon9751@naver.com>
2022-06-05 09:19:34 +0900

    주문 조회 로직을 추가하라
    주문 조회 로직을 추가합니다. 반환형을 dto로 개선해야 합니다

--- app/src/main/java/com/example/bookclub/controller/api/OrderApiController.java
+++ app/src/main/java/com/example/bookclub/controller/api/OrderApiController.java
@@ -2,8 +2,11 @@ package com.example.bookclub.controller.api;
 
 import com.example.bookclub.application.order.OrderService;
 import com.example.bookclub.common.response.CommonResponse;
+import com.example.bookclub.domain.order.Order;
 import com.example.bookclub.dto.OrderDto;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -26,5 +29,10 @@ public class OrderApiController {
 	) {
 		OrderDto.OrderCreateResponse response = orderService.create(orderCreateRequest);
 		return CommonResponse.success(response);
+	}
+
+	@GetMapping("/{id}")
+	public Order detail(@PathVariable Long id) {
+		return orderService.detail(id);
 	}
 }
