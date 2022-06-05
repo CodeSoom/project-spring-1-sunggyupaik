dd55ab9013686b76a9d55288a359119c96242f26
sunggyupaik <melon9751@naver.com>
2022-06-05 12:49:31 +0900

    결제 요청을 추가하라
    주문에 대한 결제 요청을 추가합니다. 나이스페이를 연동하기 위해서 요청과
    서비스를 만듭니다. 구체적인 구현을 해야합니다

--- app/src/main/java/com/example/bookclub/application/order/payment/PaymentService.java
+++ app/src/main/java/com/example/bookclub/application/order/payment/PaymentService.java
@@ -1,14 +1,27 @@
 package com.example.bookclub.application.order.payment;
 
+import com.example.bookclub.domain.order.payment.PayMethod;
 import com.example.bookclub.dto.OrderDto;
+import com.example.bookclub.infrastructure.order.payment.NicePayProcessor;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.Objects;
 
 @Service
 public class PaymentService {
+	private final NicePayProcessor nicePayProcessor;
+
+	public PaymentService(NicePayProcessor nicePayProcessor) {
+		this.nicePayProcessor = nicePayProcessor;
+	}
+
 	@Transactional
 	public OrderDto.OrderPayResponse pay(OrderDto.OrderPayRequest orderPayRequest) {
+		if(Objects.equals(orderPayRequest.getPayMethod(), PayMethod.NICE_PAY.name())) {
+			nicePayProcessor.nicePayProcess(orderPayRequest);
+		}
+
 		return null;
 	}
 }
---
+++ app/src/main/java/com/example/bookclub/infrastructure/order/payment/NicePayProcessor.java
@@ -0,0 +1,11 @@
+package com.example.bookclub.infrastructure.order.payment;
+
+import com.example.bookclub.dto.OrderDto;
+import org.springframework.stereotype.Component;
+
+@Component
+public class NicePayProcessor {
+	public void nicePayProcess(OrderDto.OrderPayRequest orderPayRequest) {
+
+	}
+}
