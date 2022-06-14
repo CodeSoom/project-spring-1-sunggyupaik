edde0eabb953363cd48ebe866c85a9fbe37a9d9a
sunggyupaik <melon9751@naver.com>
2022-06-14 18:56:06 +0900

    Diary 도메인을 개선하라
    id를 Long 타입으로 바꾸고 자동으로 1씩 증가하도록 개선합니다

--- app/src/main/java/com/example/bookclub/domain/post/Diary.java
+++ app/src/main/java/com/example/bookclub/domain/post/Diary.java
@@ -8,6 +8,7 @@ import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
@@ -15,13 +16,13 @@ import javax.persistence.Id;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @ToString(callSuper = true)
 public class Diary {
-	@Id
-	private String id;
+	@Id @GeneratedValue
+	private Long id;
 
 	private String content;
 
 	@Builder
-	public Diary(String id, String content) {
+	public Diary(Long id, String content) {
 		this.id = id;
 		this.content = content;
 	}
