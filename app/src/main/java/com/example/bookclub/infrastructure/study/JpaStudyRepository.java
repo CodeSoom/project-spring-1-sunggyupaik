7be2b3eadc86d4a0595ac5aba5af4676b3c97aca
sunggyupaik <melon9751@naver.com>
2022-08-23 21:11:26 +0900

    스터디 신청에 잠금을 추가하라
    스터디 신청에 정원을 초과하는 동시성 이슈가 생길 수 있으므로 비관적
    잠금으로 스터디 신청을 추가합니다

--- app/src/main/java/com/example/bookclub/infrastructure/study/JpaStudyRepository.java
+++ app/src/main/java/com/example/bookclub/infrastructure/study/JpaStudyRepository.java
@@ -2,13 +2,20 @@ package com.example.bookclub.infrastructure.study;
 
 import com.example.bookclub.domain.study.Study;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
 
+import javax.persistence.LockModeType;
 import java.util.List;
 import java.util.Optional;
 
 public interface JpaStudyRepository
         extends StudyRepositoryCustom, JpaRepository<Study, Long> {
     Optional<Study> findById(Long id);
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("select s from Study s where s.id = :id")
+    Optional<Study> findByIdForUpdate(Long id);
 
     Optional<Study> findByEmail(String email);
 
