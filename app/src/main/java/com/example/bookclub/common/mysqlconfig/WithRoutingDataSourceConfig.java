d6a4f5947cc083c2d249e348441bc5b38baf7a73
sunggyupaik <melon9751@naver.com>
2022-09-23 22:30:19 +0900

    #105-db 레플리카 설정을 개선하라
    쓰기와 읽기 전용 데이터 베이스를
    LazyConnectionDataSourceProxy를 사용하여
    실제 db 작동 시, 적절하게 라우팅 되도록
    설정합니다

--- app/src/main/java/com/example/bookclub/common/mysqlconfig/WithRoutingDataSourceConfig.java
+++ app/src/main/java/com/example/bookclub/common/mysqlconfig/WithRoutingDataSourceConfig.java
@@ -1,10 +1,13 @@
 package com.example.bookclub.common.mysqlconfig;
 
 import com.zaxxer.hikari.HikariDataSource;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.LazyConnectionDataSourceProxy;
 import org.springframework.transaction.PlatformTransactionManager;
@@ -13,38 +16,42 @@ import javax.sql.DataSource;
 import java.util.HashMap;
 import java.util.Map;
 
-//@Configuration
+/**
+ * 읽기, 쓰기 저장소를 설정하고 경우에 따라 라우팅되도록 한다
+ */
+@Configuration
+@Slf4j
 public class WithRoutingDataSourceConfig {
+	/**
+	 * 주어진 사용자 식별자와 사용자 이메일로 인코딩 된 토큰을 생성하고 반환한다.
+	 *
+	 * @return 쓰기 저장소
+	 */
 	@Bean
+	@Primary
 	@ConfigurationProperties(prefix = "spring.datasource.hikari.write")
 	public DataSource writeDataSource() {
-		return DataSourceBuilder
-				.create()
-				.type(HikariDataSource.class)
-				.build();
-	}
-
-	@Bean
-	@ConfigurationProperties(prefix = "spring.datasource.hikari.read")
-	public DataSource readDataSource() {
-		return DataSourceBuilder
-				.create()
-				.type(HikariDataSource.class)
-				.build();
+		return DataSourceBuilder.create().type(HikariDataSource.class).build();
 	}
 
 	/**
-	 * {@link org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource}는
-	 * {@link org.springframework.beans.factory.InitializingBean}을 구현하므로,
-	 * 명시적으로 afterPropertiesSet()메소드를 호출하거나
-	 * 별도 @Bean으로 만들어 Spring Life Cycle을 타도록 해야 한다.
+	 * 주어진 사용자 식별자와 사용자 이메일로 인코딩 된 토큰을 생성하고 반환한다.
+	 *
+	 * @return 읽기 저장소
 	 */
+	@Bean
+	@ConfigurationProperties(prefix = "spring.datasource.hikari.read")
+	public DataSource readDataSource() {
+		return DataSourceBuilder.create().type(HikariDataSource.class).build();
+	}
+
+
 	@Bean
 	public DataSource routingDataSource(@Qualifier("writeDataSource") DataSource writeDataSource,
 										@Qualifier("readDataSource") DataSource readDataSource) {
 		ReplicationRoutingDataSource routingDataSource = new ReplicationRoutingDataSource();
 
-		Map<Object, Object> dataSourceMap = new HashMap<Object, Object>();
+		Map<Object, Object> dataSourceMap = new HashMap<>();
 		dataSourceMap.put("write", writeDataSource);
 		dataSourceMap.put("read", readDataSource);
 		routingDataSource.setTargetDataSources(dataSourceMap);
