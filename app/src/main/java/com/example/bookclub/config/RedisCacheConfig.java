a9c5b6e47db9ccbd2c79c04832a0b4147ae43c24
sunggyupaik <melon9751@naver.com>
2022-10-01 11:41:49 +0900

    [#111] 레디스 serialize 설정을 개선하라
    기존에 `objectMapper`로 serialize를 하면
    캐시 설정으로 인터뷰 리스트를 deserialize를 할 때,
    오류가 납니다
    
    이를 해결하기 위해서 `RedisSerializer.java()`를
    사용해서 java serialize를 사용하도록 합니다
    
    또한 `InterviewResultDto`가 `RedisSerialize.java()`로
    정상 작동하기 위해 `Serialize`를 구현하도록 합니다

--- app/src/main/java/com/example/bookclub/config/RedisCacheConfig.java
+++ app/src/main/java/com/example/bookclub/config/RedisCacheConfig.java
@@ -1,6 +1,5 @@
 package com.example.bookclub.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,28 +8,20 @@ import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+import org.springframework.data.redis.serializer.RedisSerializer;
 
 import java.time.Duration;
 import java.util.HashMap;
 import java.util.Map;
 
 @Configuration
-@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 1800)
 public class RedisCacheConfig {
 	@Value("${spring.redis.cache.host}")
 	private String hostName;
 
 	@Value("${spring.redis.cache.port}")
 	private int port;
-
-	private final ObjectMapper objectMapper;
-
-	public RedisCacheConfig(ObjectMapper objectMapper) {
-		this.objectMapper = objectMapper;
-	}
 
 	@Bean
 	public RedisConnectionFactory redisCacheConnectionFactory() {
@@ -46,9 +37,7 @@ public class RedisCacheConfig {
 				.defaultCacheConfig()
 				.disableCachingNullValues()
 				.serializeValuesWith(
-						RedisSerializationContext.SerializationPair
-								.fromSerializer(new GenericJackson2JsonRedisSerializer(objectMapper))
-				);
+						RedisSerializationContext.SerializationPair.fromSerializer(RedisSerializer.java()));
 
 		Map<String, RedisCacheConfiguration> redisCacheConfigurationMap = new HashMap<>();
 		redisCacheConfigurationMap.put("Interviews", redisCacheConfiguration.entryTtl(Duration.ofMinutes(5)));
