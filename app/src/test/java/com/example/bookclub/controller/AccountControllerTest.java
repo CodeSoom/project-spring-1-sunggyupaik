21566b92436b97feb63d5a50f2423a8f682d9848
sunggyupaik <melon9751@naver.com>
2022-10-04 20:24:02 +0900

    [#114] 즐겨찾기 이동 요청 테스트를 추가하라
    즐겨찾기 이동 요청 테스트를 추가합니다
    
    1. 로그인이 된 경우
    2. 로그인이 안 된 경우
    3. 로그인이 되었으나 자신의 식별자가 아닌 경우
    
    3가지를 테스트합니다

--- app/src/test/java/com/example/bookclub/controller/AccountControllerTest.java
+++ app/src/test/java/com/example/bookclub/controller/AccountControllerTest.java
@@ -2,12 +2,14 @@ package com.example.bookclub.controller;
 
 import com.example.bookclub.application.account.AccountAuthenticationService;
 import com.example.bookclub.application.account.AccountService;
+import com.example.bookclub.application.study.query.StudyQueryService;
 import com.example.bookclub.application.uploadfile.UploadFileService;
 import com.example.bookclub.domain.account.Account;
 import com.example.bookclub.infrastructure.study.JpaStudyRepository;
 import com.example.bookclub.security.CustomDeniedHandler;
 import com.example.bookclub.security.CustomEntryPoint;
 import com.example.bookclub.security.PersistTokenRepository;
+import com.example.bookclub.security.UserAccount;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -17,28 +19,44 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.filter.CharacterEncodingFilter;
 
 import javax.sql.DataSource;
+import java.util.ArrayList;
+import java.util.List;
 
+import static org.hamcrest.Matchers.containsString;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 @WebMvcTest(AccountController.class)
 public class AccountControllerTest {
 	private static final String EMAIL = "1234@naver.com";
+
 	private Account account;
+	private UserAccount userAccount;
+	private UsernamePasswordAuthenticationToken accountToken;
 
 	@Autowired
 	MockMvc mockMvc;
 
 	@Autowired
 	private WebApplicationContext ctx;
+
+	@MockBean
+	private StudyQueryService studyQueryService;
 
 	@MockBean
 	private AccountAuthenticationService accountAuthenticationService;
@@ -76,6 +94,21 @@ public class AccountControllerTest {
 		account = Account.builder()
 				.id(1L)
 				.build();
+
+		List<GrantedAuthority> USER_AUTHORITY = new ArrayList<GrantedAuthority>();
+		USER_AUTHORITY.add(new SimpleGrantedAuthority("USER"));
+
+		List<GrantedAuthority> USER_ANONYMOUS = new ArrayList<GrantedAuthority>();
+		USER_AUTHORITY.add(new SimpleGrantedAuthority("ROLE_ANONYMOUS"));
+
+		userAccount = UserAccount.builder()
+				.account(account)
+				.authorities(USER_AUTHORITY)
+				.build();
+
+		accountToken = new UsernamePasswordAuthenticationToken(
+				userAccount, null, USER_AUTHORITY
+		);
 	}
 
 	@Nested
@@ -107,6 +140,63 @@ public class AccountControllerTest {
 						.andExpect(status().isOk())
 						.andExpect(content().contentType(MediaType.valueOf("text/html;charset=UTF-8")))
 						.andExpect(view().name("users/users-save"));
+			}
+		}
+	}
+
+	@Nested
+	@DisplayName("accountFavorite 메서드는")
+	class Describe_accountFavorite {
+		@Nested
+		@DisplayName("만약 로그인한 사용자가 주어진다면")
+		class Context_WithAccount {
+			@Test
+			@DisplayName("즐겨찾기 화면을 리턴한다")
+			void itReturnsUsersFavoriteView() throws Exception {
+				SecurityContextHolder.getContext().setAuthentication(accountToken);
+
+				mockMvc.perform(get("/users/{id}/favorite", 1L)
+								.param("userAccount", objectMapper.writeValueAsString(userAccount))
+						)
+						.andExpect(status().isOk())
+						.andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+						.andExpect(model().attributeExists("StudyFavoriteDto"))
+						.andExpect(view().name("users/users-favorite"));
+			}
+		}
+
+		@Nested
+		@DisplayName("만약 로그인한 사용자가 주어지지 않는다면")
+		class Context_WithoutAccount {
+			@Test
+			@DisplayName("로그인이 필요하다는 오류메세지를 리턴한다")
+			@WithMockUser(username = "test", password = "password", roles = "ANONYMOUS")
+			void itReturnsFailedToEvaluateExpressionMessage() throws Exception {
+				mockMvc.perform(get("/users/{id}/favorite", 1L))
+						.andDo(print())
+						.andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+						.andExpect(content().string(containsString("Failed to evaluate expression")))
+						.andExpect(status().is5xxServerError());
+			}
+		}
+
+		@Nested
+		@DisplayName("만약 로그인한 사용자와 이동하려는 즐겨찾기 페이지에 다른 식별자가 주어진다면")
+		class Context_WithNotMineFavoriteAccountId {
+			private final Long FAVORITE_ID = 3L;
+
+			@Test
+			@DisplayName("접근이 불가능하다는 오류메세지를 리턴한다")
+			void itReturnsAccessIsDeniedMessage() throws Exception {
+				SecurityContextHolder.getContext().setAuthentication(accountToken);
+
+				mockMvc.perform(get("/users/{id}/favorite", FAVORITE_ID)
+								.param("userAccount", objectMapper.writeValueAsString(userAccount))
+						)
+						.andDo(print())
+						.andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+						.andExpect(content().string(containsString("Access is denied")))
+						.andExpect(status().is5xxServerError());
 			}
 		}
 	}
