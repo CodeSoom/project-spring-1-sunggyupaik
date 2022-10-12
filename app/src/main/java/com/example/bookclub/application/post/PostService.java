0092c2f7980705951fb08e2aa4bf5069812a153f
sunggyupaik <melon9751@naver.com>
2022-10-12 22:01:14 +0900

    [#114] 한줄 게시판에 javaDocs를 추가하라
    한 줄 게시판에 javaDocs를 추가합니다
    
    게시판 조회하는 `Post`를 규칙에 따라 `post`로
    수정합니다

--- app/src/main/java/com/example/bookclub/application/post/PostService.java
+++ app/src/main/java/com/example/bookclub/application/post/PostService.java
@@ -9,6 +9,9 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+/**
+ * 한줄 게시글 저장, 게시글 검색, 게시글 전체 조회를 한다.
+ */
 @Service
 public class PostService {
 	private final ElasticPostRepository elasticPostRepository;
@@ -20,26 +23,53 @@ public class PostService {
 		this.diaryRepository = diaryRepository;
 	}
 
+	/**
+	 * 주어진 한줄 게시글을 저장한다
+	 *
+	 * @param post 한줄 게시글
+	 */
 	@Transactional
 	public void create(Post post) {
 		elasticPostRepository.save(post);
 	}
 
+	/**
+	 * 주어진 한줄 게시글을 저장한다
+	 *
+	 * @param diary 한줄 게시글
+	 */
 	@Transactional
 	public void createDiary(Diary diary) {
 		diaryRepository.save(diary);
 	}
 
+	/**
+	 * 주어진 한줄 게시글을 저장한다
+	 *
+	 * @param diary 한줄 게시글
+	 * @return 생성된 한줄 게시글
+	 */
 	@Transactional
 	public Diary createDiaryRaw(Diary diary) {
 		return diaryRepository.save(diary);
 	}
 
+	/**
+	 * 주어진 검색어에 해당하는 한줄 게시글 리스트를 반환한다
+	 *
+	 * @param content 내용
+	 * @return 한줄 게시글 리스트
+	 */
 	@Transactional(readOnly = true)
 	public List<Post> findByContent(String content) {
 		return elasticPostRepository.findByContent(content);
 	}
 
+	/**
+	 * 한줄 게시글 리스트를를 반환한다
+	 *
+	 * @return 한줄 게시글 리스트
+	 */
 	@Transactional(readOnly = true)
 	public List<Post> listAll() {
 		return elasticPostRepository.findAll();
--- app/src/main/java/com/example/bookclub/controller/PostController.java
+++ app/src/main/java/com/example/bookclub/controller/PostController.java
@@ -9,6 +9,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
+/**
+ * 한줄 게시판 조회 페이지를 요청한다
+ */
 @Controller
 public class PostController {
 	private final PostService postService;
@@ -17,8 +20,15 @@ public class PostController {
 		this.postService = postService;
 	}
 
+	/**
+	 * 한줄 게시판 페이지로 이동한다
+	 *
+	 * @param model 모델
+	 * @param content 검색 내용
+	 * @return 한줄 게시판 페이지
+	 */
 	@GetMapping("/posts")
-	public String Post(Model model,
+	public String post(Model model,
 					   @RequestParam(required = false, defaultValue = "") String content
 	) {
 		List<Post> posts = null;
